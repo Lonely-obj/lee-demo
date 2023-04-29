2c8d4a7613ff7f67a1cd33b379e78397723f7d64
leeron <leeron6@163.com>
2023-04-29 13:54:21 +0800

    '项目新建'

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'index'
+})
+</script>
+
+<template>
+  <div class="login">
+    <h1>Login</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
